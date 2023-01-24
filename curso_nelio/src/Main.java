c6b318c000794403119f44f6c4f97edf3d70dcd0
LuanAlbis <luanalbis12@gmail.com>
2023-01-23 21:16:34 -0300

    att

---
+++ curso_nelio/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int idade = 25;
+		System.out.println("Olá Mundo!"); // aspas duplas
+		System.out.println("Eu quero comer minha namorada"); // ponto e virgula no final
+		System.out.println(idade);
+		
+		
+		
+		
+	}
+
+}
+
