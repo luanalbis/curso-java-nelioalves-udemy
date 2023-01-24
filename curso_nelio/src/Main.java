4729850fd1501ce55ddbedaaad0e8c55615191fc
LuanAlbis <luanalbis12@gmail.com>
2023-01-24 10:36:25 -0300

    Update Main.java

--- curso_nelio/src/Main.java
+++ curso_nelio/src/Main.java
@@ -2,7 +2,14 @@
 public class Main {
 
 	public static void main(String[] args) {
-		int idade = 25;
+		// existem varios tipos primitivos de váriaveis
+		int idade = 25; // variáveis declaradas com o tipo numero
+		double flutuante = 1.50; // váriaveis do tipo flutuante
+		char sexo = 'f';
+		String nomes = "luan";
+		
+		// variaveis possuem - Nome - Tipo - Valor - endereço
+		
 		System.out.println("Olá Mundo!"); // aspas duplas
 		System.out.println("Eu quero comer minha namorada"); // ponto e virgula no final
 		System.out.println(idade);
